3246898d61aa55473527872e5cadc22826463af6
okuhara <okuhara@amy.hi-ho.ne.jp>
2023-12-04 14:33:33 +0900

    Exclude hash init time from count games; other minor size opts

--- src/board.h
+++ src/board.h
@@ -31,7 +31,7 @@ void board_init(Board*);
 int board_set(Board*, const char*);
 int board_from_FEN(Board*, const char*);
 int board_compare(const Board*, const Board*);
-bool board_equal(const Board*, const Board*);
+bool board_lesser(const Board*, const Board*);
 void board_symetry(const Board*, const int, Board*);
 int board_unique(const Board*, Board*);
 void board_check(const Board*);
