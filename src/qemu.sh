1bcc82979072ccba9c867386eb022c39b877c499
Toshihiko Okuhara <okuhara@amy.hi-ho.ne.jp>
2025-06-11 18:39:12 +0900

    add flto=auto; fix qemu for sve256

--- src/qemu.sh
+++ src/qemu.sh
@@ -5,10 +5,10 @@ cd ../bin
 qemu-arm -L /usr/arm-linux-gnueabi ./lEdax-arm-neon -n 1 -l 60 -solve ../problem/fforum-20-39.obf
 elif [ "$1" = sve ]
 then
-make build OS=linux ARCH=arm-sve COMP=gcc CC=aarch64-linux-gnu-gcc
+make build OS=linux ARCH=arm-sve256 COMP=gcc CC=aarch64-linux-gnu-gcc
 # clang --target=aarch64-linux-gnu -std=c99 -pedantic -W -Wall -Wextra -pipe -D_GNU_SOURCE=1 -DUNICODE -Ofast -D NDEBUG -march=armv8.2-a+sve -flto all.c -s -o ../bin/lEdax-armv8+sve -lm -lrt -lpthread
 cd ../bin
-qemu-aarch64 -L /usr/aarch64-linux-gnu -cpu max,sve128=on ./lEdax-arm-sve -n 1 -l 60 -solve ../problem/fforum-20-39.obf
+qemu-aarch64 -L /usr/aarch64-linux-gnu -cpu max,sve256=on ./lEdax-arm-sve256 -n 1 -l 60 -solve ../problem/fforum-20-39.obf
 else
 make build OS=linux ARCH=arm COMP=gcc CC=aarch64-linux-gnu-gcc
 # clang --target=aarch64-linux-gnu -std=c99 -pedantic -W -Wall -Wextra -pipe -D_GNU_SOURCE=1 -DUNICODE -Ofast -D NDEBUG -march=armv8-a -flto all.c -s -o ../bin/lEdax-armv8 -lm -lrt -lpthread
