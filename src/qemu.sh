d2f0d9dbc24a233be7cb8d831e30aeddc3c122bc
Toshihiko Okuhara <okuhara@amy.hi-ho.ne.jp>
2025-06-24 18:50:31 +0900

    split neon build from arm32 in qemu.sh

--- src/qemu.sh
+++ src/qemu.sh
@@ -1,5 +1,10 @@
 if [ "$1" = arm32 ]
 then
+make build OS=linux ARCH=arm COMP=gcc CC=arm-linux-gnueabi-gcc
+cd ../bin
+qemu-arm -L /usr/arm-linux-gnueabi -cpu max,neon=off ./lEdax-arm -n 1 -l 60 -solve ../problem/fforum-20-39.obf
+elif [ "$1" = neon ]
+then
 make build OS=linux ARCH=arm-neon COMP=gcc CC=arm-linux-gnueabi-gcc
 cd ../bin
 qemu-arm -L /usr/arm-linux-gnueabi ./lEdax-arm-neon -n 1 -l 60 -solve ../problem/fforum-20-39.obf
@@ -10,10 +15,10 @@ make build OS=linux ARCH=arm-sve256 COMP=gcc CC=aarch64-linux-gnu-gcc
 cd ../bin
 qemu-aarch64 -L /usr/aarch64-linux-gnu -cpu max,sve256=on ./lEdax-arm-sve256 -n 1 -l 60 -solve ../problem/fforum-20-39.obf
 else
-make build OS=linux ARCH=arm COMP=gcc CC=aarch64-linux-gnu-gcc
+make build OS=linux ARCH=armv8 COMP=gcc CC=aarch64-linux-gnu-gcc
 # clang --target=aarch64-linux-gnu -std=c99 -pedantic -W -Wall -Wextra -pipe -D_GNU_SOURCE=1 -DUNICODE -Ofast -D NDEBUG -march=armv8-a -flto all.c -s -o ../bin/lEdax-armv8 -lm -lrt -lpthread
 cd ../bin
-qemu-aarch64 -L /usr/aarch64-linux-gnu ./lEdax-arm -n 1 -l 60 -solve ../problem/fforum-20-39.obf
+qemu-aarch64 -L /usr/aarch64-linux-gnu ./lEdax-armv8 -n 1 -l 60 -solve ../problem/fforum-20-39.obf
 fi
 cd ../src
 # C:\Android\android-ndk-r21\toolchains\llvm\prebuilt\windows-x86_64\bin\clang.exe --target=aarch64-linux-gnu -O2 -S flip_neon_bitscan.c
