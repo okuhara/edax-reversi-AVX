1d6aca3acdd6b11bd10b06f186f63c0f131e55f6
Toshihiko Okuhara <okuhara@amy.hi-ho.ne.jp>
2025-12-22 19:01:18 +0900

    adopt Android 16KB page size

--- src/Android.mk
+++ src/Android.mk
@@ -3,6 +3,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := aEdax # should be renamed to lib..aEdax..so afterwords
 LOCAL_CFLAGS += -DUNICODE
 LOCAL_SRC_FILES := all.c board_sse.c.neon eval_sse.c.neon flip_neon_bitscan.c.neon android/cpu-features.c
-LOCAL_ARM_NEON := false
+# LOCAL_ARM_NEON := false
+# LOCAL_LDFLAGS += "-Wl,-z,max-page-size=16384"	# for pre-r26 NDK
+# LOCAL_LDFLAGS += "-Wl,-z,common-page-size=16384"	# for pre-r22 NDK
 # cmd-strip :=
 include $(BUILD_EXECUTABLE)
